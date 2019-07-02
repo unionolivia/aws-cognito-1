c019a337ef3123f28295be0a12c66e5cb8fa4414
unionolivia <unionolivia@gmail.com>
2019-07-02 18:14:38 +0100

    remove keys stuff

--- params.php
+++ params.php
@@ -2,13 +2,13 @@
 
 return [
     'credentials' => [
-        'key' => 'AKIAYWDR7YDCROJTXCNF',
-        'secret' => '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',
+        'key' => '',
+        'secret' => '',
     ],
     'region' => 'eu-west-1',
     'version' => 'latest',
 
-    'app_client_id' => '6pk1hd4q18qa96cu1nj521p64m',
+    'app_client_id' => '',
     'app_client_secret' => '',
-    'user_pool_id' => 'eu-west-1_4MtWNKOIc',
+    'user_pool_id' => '',
 ];
